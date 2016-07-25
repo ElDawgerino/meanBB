2f9615a2c9415f986e0eecb635131a2fad1ef5cc
ElDawgerino <bouhahah@gmail.com>
2016-07-25 17:05:14 +0200

    List of users is now displayed on admin page

--- public/javascripts/adminApp.js
+++ public/javascripts/adminApp.js
@@ -1,14 +1,32 @@
 var app = angular.module('meanBB-admin', ['ui.router', 'angular-jwt', 'meanBB-sharedElements']);
 
+app.factory('adminNet', [
+    '$http',
+    function($http){
+        var o = {};
+
+        o.getUsers = function(callback){
+            return $http.get('admin/users').success(function(data){
+                callback(data);
+            });
+        };
+
+        return o;
+}]);
+
 app.controller('MainAdminCtrl', [
   '$scope',
   'page',
   'auth',
-  function($scope, page, auth){
+  'adminNet',
+  function($scope, page, auth, adminNet){
     $scope.admin = auth.isAdmin()
     if(!$scope.admin || ($scope.admin == undefined)){
         page.setTitle("Access Denied");
     } else {
         page.setTitle("Admin Panel");
+        adminNet.getUsers(function(data){
+            $scope.users = data;
+        });
     }
 }]);
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
+var passport = require('passport');
+var jwt = require('express-jwt');
 var auth = jwt({secret: 'C7F209A547201848BB7BD887A18D9FD7D45D14EE293614C09A734DA4A389E589', userProperty: 'payload'});
 
 /* GET admin page. */

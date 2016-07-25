898218dfa5c824eab98e1e251e096df8ab21b1bf
ElDawgerino <bouhahah@gmail.com>
2016-07-25 16:43:07 +0200

    Ajout d'une route admin/users

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var User = mongoose.model('User');
+
+var auth = jwt({secret: 'C7F209A547201848BB7BD887A18D9FD7D45D14EE293614C09A734DA4A389E589', userProperty: 'payload'});
+
+/* GET admin page. */
 router.get('/', function(req, res, next) {
   res.render('admin', {})
+});
+
+/* GET user list */
+router.get('/users', auth, function(req, res, next) {
+    //Checks if the user is admin
+    if(req.payload.admin){
+        User.find(function(err, users){
+            if(err){
+                return next(err);
+            }
+            res.json(users);
+        });
+    } else {
+        return res.status(403);
+    }
 });
 
 module.exports = router;
